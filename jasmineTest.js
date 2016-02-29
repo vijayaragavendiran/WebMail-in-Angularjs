aeab9ca904a753124c98feac2ec4c32acd9e3da7
Vijayaragavendiran <vijayaragavendiran@outlook.com>
2016-02-29 15:22:04 -0800

    Update jasmineTest.js

--- jasmineTest.js
+++ jasmineTest.js
@@ -1,3 +1,11 @@
+///<reference path="app.js" />
+///<reference path="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js" />
+///<reference path="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" />
+///<reference path="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" />
+///<reference path="https://cdnjs.cloudflare.com/ajax/libs/ag-grid/3.3.3/ag-grid.min.js" />
+///<reference path="" />
+///<reference path="" />
+
 describe(‘Office Mail Test’, function() {
 
 	beforeEach(module('app'));
