3f7e623ce0df66498556ed701ef1c4dab440726a
vijayaragavendiran <vijayaragavendiran@outlook.com>
2016-03-02 23:14:43 -0800

    Included comments

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 agGrid.initialiseAgGridWithAngular1(angular);
 
-var app = angular.module('app',["agGrid"]);
-app.controller('officeMailController',['$scope',function($scope){
+var app = angular.module('app',["agGrid"]);// Declared module
+app.controller('officeMailController',['$scope',function($scope){ // Declared Controller and DI 
 
 
 $scope.name = 'r';
 
 $scope.loadSentItems = function(){
 
-	
+	//Code here to load send items
 
 }
 
 $scope.loadInbox = function(){
     $scope.test=1;
-
+//Column definition of the ag grid
 	var columnDefs = [
         {headerName: "Date", field: "date",width: 100},
         {headerName: "Subject", field: "subject",width: 650},
         {headerName: "From", field: "from",width: 250}
     ];
-
+//inbox mail details
     var rowData = [
         {date: "02/25/2016", subject: "welcome to adidas || Upto 50 percent off", from: 'Adidas', to:'Vijay'},
         {date: "02/5/2016", subject: "Your application for Sr. Software Engineer (Javascript)  - Highlights for Children  has been viewed", from: 'LinkedIn',to:'Vijay'},
@@ -36,7 +36,7 @@ $scope.loadInbox = function(){
         {date: "01/1/2016", subject: "Recharge with MobiKwik and get a chance to win a smart watch, smart phone & smart headphones!", from: 'Walmart',to:'Vijay'},
         {date: "02/22/2016", subject: "welcome to adidas || Upto 50 percent off", from: 'Adidas',to:'Vijay'}
     ];
-	$scope.inboxCount = rowData.length;
+	$scope.inboxCount = rowData.length;// Show inbox total mail
     $scope.gridOptions = {
         columnDefs: columnDefs,
         rowSelection: 'single',
